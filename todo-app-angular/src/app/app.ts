065e9ca813b9c849de1b50942bd2f3a948af6749
reymarkvictorino <victorinoreymark1990@gmail.com>
2025-07-29 10:47:56 +0800

    change color of the table

--- todo-app-angular/src/app/app.ts
+++ todo-app-angular/src/app/app.ts
@@ -2,19 +2,20 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ColorSketchModule } from 'ngx-color/sketch';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [RouterOutlet, CommonModule, FormsModule,ColorSketchModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('todo-app-angular');
   tableData = [
-    { id: 1, name: 'Task 1', description: 'Complete the Angular project', completed: '' , Status: '' },
-    { id: 2, name: 'Task 2', description: 'Review the code',completed: '',Status: '' },
-    { id: 3, name: 'Task 3', description: 'Deploy the application',completed: '',Status: '' }
+    { id: 1, name: 'Task 1', description: 'Complete the Angular project', completed: '' , Status: '',color: '#ffffff' },
+    { id: 2, name: 'Task 2', description: 'Review the code',completed: '',Status: '',color: '#d4edda' },
+    { id: 3, name: 'Task 3', description: 'Deploy the application',completed: '',Status: '',color: '#d4edda' }
   ]
 
   setCompleted(id: number) {
@@ -25,4 +26,9 @@ export class App {
       console.log(`Item with id ${id} completed status set to: ${item.completed}`);
     }
   }
+
+  color = '#000';
+onColorChange(e: any) {
+  this.color = e.color.hex;
+}
 }
